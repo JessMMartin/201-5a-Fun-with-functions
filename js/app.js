df4cc462ee9db410a276279ee43a04c0876b5fa2
Jess <jessicammartin@live.co.uk>
2023-06-16 11:41:57 +0100

    lab a

--- js/app.js
+++ js/app.js
@@ -8,12 +8,13 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
+function sum(a, b) {
+     const theSum = a + b
+     const message = "The sum of " + a + " and " + b +  " is " + theSum + "."
+    return [theSum, message]
 }
-
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -26,12 +27,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
 
+function multiply(a, b) {
+    let mFunction = a * b;
+    let message = "The product of " + a + " and " + b + " is " + mFunction + ".";
+    return [mFunction, message]
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -47,12 +51,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+function sumAndMultiply(a, b, c) { 
+    function sum(a, b, c) {
+        let theSum = a, b, c;
+        let message = "The sum of " + a + " and " + b +  " is " + theSum + "."
+    }
+    function multiply(a, b, c){     
+        let mFunction = a, b, c;
+        let message2 = "The product of " + a + " and " + b + " is " + mFunction + ".";
+        return [theSum,mFunction,message,message2];
+    }
+    
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
